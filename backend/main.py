285b874466b87a55271cf4c64f1712ab71157790
malcolm <malcolm.t129@gmail.com>
2026-07-02 22:27:52 -0400

    WIP: I have the frontend able to send a post request to the backend, so now I'm working on the backend parsing and actually submitting an order.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-from ibconnect import IBAPI
+from .ibconnect import IBAPI
 
 app = FastAPI()
 
 
 #Create an object to interactive with IB TWS API
 ib_api = IBAPI()
+
+
+
+#FastAPI automatically reads a JSON payload from request body and converts it to
+#specified pydantic model. So I need to define them for the post requests.
+class OrderEntry(BaseModel):
+    ticker: str
+    exchange: str
+    secType: str
+    price: float
+    action: str
+    orderType: str 
 
 @app.get("/")
 def read_root():
@@ -27,3 +40,8 @@ def getAccountValue():
 @app.get("/api/account/portfolio")
 def getPortfolio():
     return {"res": ib_api.getPortfolio()}
+
+
+@app.post("/api/order")
+def createOrder(order: OrderEntry):
+    pass
